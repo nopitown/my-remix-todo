6592733a20802406068c08075f072f1f7116f384
Elizabeth Manrique <hey@nopitown.com>
2022-03-31 18:01:19 -0500

    make a simple index

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,21 +1,3 @@
-import { Todo } from "@prisma/client";
-import { json, useLoaderData } from "remix";
-
-type LoaderData = {
-  todos: Array<Todo>;
-};
-
-export const loader = async () => {
-  const data: LoaderData = {
-    todos: [],
-  };
-
-  return json(data);
-};
-
 export default function Index() {
-  const { todos } = useLoaderData<LoaderData>();
-
-  console.log(todos);
-  return <div></div>;
+  return <div>Hello!</div>;
 }
